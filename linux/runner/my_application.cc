225ce228cfbf0923cff0201a02fd91782fd7ad14
zivmax <zivmax@proton.me>
2026-01-22 20:29:38 +0800

    fix: update application title to "TCamp Calendar" for consistency under linux

--- linux/runner/my_application.cc
+++ linux/runner/my_application.cc
@@ -45,11 +45,11 @@ static void my_application_activate(GApplication* application) {
   if (use_header_bar) {
     GtkHeaderBar* header_bar = GTK_HEADER_BAR(gtk_header_bar_new());
     gtk_widget_show(GTK_WIDGET(header_bar));
-    gtk_header_bar_set_title(header_bar, "tcamp_calender");
+    gtk_header_bar_set_title(header_bar, "TCamp Calendar");
     gtk_header_bar_set_show_close_button(header_bar, TRUE);
     gtk_window_set_titlebar(window, GTK_WIDGET(header_bar));
   } else {
-    gtk_window_set_title(window, "tcamp_calender");
+    gtk_window_set_title(window, "TCamp Calendar");
   }
 
   gtk_window_set_default_size(window, 1280, 720);
